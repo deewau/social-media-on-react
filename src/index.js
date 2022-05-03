e7960d62ed696da102481ad3cf9f4e88c389f102
Andrew <104224221+deewau@users.noreply.github.com>
2022-05-03 03:18:45 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import state from "./Redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <App
+      dialogsData={state.dialogs}
+      messagesData={state.messages}
+      postsData={state.posts}
+    />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
